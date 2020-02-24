ef990c1c8fe33334caaac68c55db3451d8b32ec9
Tressa <merelyamaven@gmail.com>
2020-02-24 15:27:08 -0700

    Added the sections

---
+++ decaldrama/main.js
@@ -0,0 +1,9 @@
+console. log(Hi)
+
+console.log(document.querySelector(".greeting"))
+
+const greetingDiv = document.querySelector(".greeting")
+
+greetingDiv.textContent = "I just inserted text into a DOM element using my mad JavaScript skills"
+
+console.log(greetingDiv.textContent)
