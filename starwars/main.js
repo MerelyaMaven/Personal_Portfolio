f7f45690cce84837b34f0ae6e83e2527700bb21d
Tressa <merelyamaven@gmail.com>
2020-03-30 16:20:07 -0600

    Add Images to starwars

--- starwars/main.js
+++ starwars/main.js
@@ -4,8 +4,24 @@ const greetingDiv = document.querySelector('.greeting')
 
 const castList = document.createElement("ul")
 
-const listItem1 = document.createElement("li")
+let counter = 1
+people.forEach(person => {
+    let listItem = document.createElement("li") 
+    listItem.textContent = person.name
+    castList.appendChild(listItem)
 
-castList.appendChild(listItem1)
+    let anchorWrap = document.createElement("a")
+    anchorWrap.href = "#"
 
+    let imageItem = document.createElement("img")
+    imageItem.src =  `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    
+    //Add user clicks
+    imageItem.addEventListener("click", () => {
+        console.log("It worked")
+    })
+    anchorWrap.appendChild(imageItem)
+    greetingDiv.appendChild(anchorWrap)
+    counter++
+});
 greetingDiv.appendChild(castList)
