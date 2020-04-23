aedf7f7e5a9bb8114aa6354a21737002fa86994e
Tressa <merelyamaven@gmail.com>
2020-04-23 15:57:02 -0600

    Added the card to baseball

--- baseball/index.js
+++ baseball/index.js
@@ -1,16 +1,36 @@
-function getBaseballData(url) {
-    let baseballGrid = document.querySelector('.baseballGrid')
-    fetch(url).then(function(response) {
-        response.json().then(function (data) {
-            console.log(data)
-            //baseballGrid.textContent = data.name
-        })
-    })
+function getPokeData(url) {
+	fetch(url).then(function (response) {
+		response.json().then(function (data) {
+			console.log(data)
+			populatePokeCards(data)
+		})
+	})
 }
 
-getBaseballData('https://fantasybaseballnerd.com/service/players')
+let pokemonGrid = document.querySelector('.pokemonGrid')
 
-var card = document.querySelector('.card')
-card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-})
+getPokeData('https://pokeapi.co/api/v2/pokemon/1/')
+
+function populatePokeCards(pokeArray) {
+	let pokeScene = document.createElement('div')
+	pokeScene.className = 'scene'
+	let pokeCard = document.createElement('div')
+	pokeCard.className = 'card'
+	pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
+	let pokeFront = document.createElement('div')
+	pokeFront.className = 'card__face card__face--front'
+	pokeCard.textContent = 'front'
+	let pokeBack = document.createElement('div')
+	pokeBack.className = 'card__face card__face--back'
+	pokeBack.textContent = 'back'
+
+	pokeCard.appendChild(pokeFront)
+	pokeCard.appendChild(pokeBack)
+	pokeScene.appendChild(pokeCard)
+	pokemonGrid.appendChild(pokeScene)
+}
+
+
+
+
+
--- congress/main.js
+++ congress/main.js
@@ -15,7 +15,9 @@ function getSimplifiedSenators(senatorArray) {
         id: senator.id,
         name: `${senator.first_name}${middleName}${senator.last_name}`,
         imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
-        seniority: parseInt(senator.seniority, 10)
+        seniority: parseInt(senator.seniority, 10),
+        missedVotesPct: senator.missed_votes_pct,
+        party: senator.party,
         date_of_birth: parseInt(senator.date_of_birth, 10)
     }
 })
@@ -35,13 +37,49 @@ function populateSenatorDiv(simpleSenators) {
         figImg.src = senator.imgURL
         figCaption.textContent = senator.name
 
+        figCaption.appendChild(partyIcon)
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
         senatorDiv.appendChild(senFigure)
-        
-
+        senDiv.appendChild(progressBars(senator))
+        senatorGrid.appendchild(senDiv)
     })
-    
+}
+
+function progressBars(senator) {
+    let progressDiv = document.createElement('div')
+    progressDiv.className = 'progressDiv'
+    let loyaltyLabel = document.createElement('label')
+    loyaltyLabel.for = 'loyalty'
+    loyaltyLabel.textContent = 'Loyalty'
+    let loyaltyBar = document.createElement('progress')
+    loyaltyBar.id = 'loyalty'
+    loyaltyLabel.max = 100
+    loyaltyBar.value = senator.loyaltyPct
+    let seniorityLabel = document.createElement('label')
+    seniorityLabel.for = 'seniority'
+    seniorityLabel.textContent = 'Seniority'
+    let seniorityBar = document.createElement('progress')
+    seniorityBar.id = 'seniority'
+    seniorityBar.max = 100
+    seniorityBar.value = parseInt((senator.seniority / mostSeniority.seniority) * 100)
+    let votingLabel = document.createElement('label')
+    votingLabel.for = 'voting'
+    votingLabel.textContent = 'Vote'
+    let VotingBar = document.createElement('progress')
+    votingLabel.id ='voting'
+    votingBar.max = 100
+    votingBar.value = 10
+
+    progressDiv.appendChild(loyaltyLabel)
+    progressDiv.appendChild(loyaltyBar)
+    progressDiv.appendChild(seniorityLabel)
+    progressDiv.appendChild(seniorityBar)
+    progressDiv.appendChild(votingLabel)
+    progressDiv.appendChild(lvotingBar)
+    return progressDiv
+
+
 }
 
 const filterSenators = (prop, value) => {
@@ -59,6 +97,14 @@ const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) => a
 
 const missedVotes = getSimplifiedSenators(senators).reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
 
+let loyalArray = []
+
+const mostLoyal = getSimplifiedSenators(republicans).reduce((acc, senator) => {
+    if(senator.loyaltyPct === 100) {
+        loyalArray.push(senator)
+    }
+    return acc.loytyPct > senator.loyaltyPct ? acc : senator
+})
 
 //Sort by value
 function senioritySort() {
