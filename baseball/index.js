365bcad9fd4a8b3773e1e10bc52a54b736113a28
Tressa <merelyamaven@gmail.com>
2020-04-23 20:17:16 -0600

    images from franzy

--- baseball/index.js
+++ baseball/index.js
@@ -1,33 +1,30 @@
-const allPokemon =[]
-
-function getPokeData(url) {
-	fetch(url).then(function (response) {
-		response.json().then(function (pokeData) {
-			console.log(pokeData.results)
-			const pokeMap = pokeData.results.map(pokemon => {
-				return fetch(pokemon.url).then(resData => {
-					resData.json().then(pokeJson => {
-						allPokemon.push(pokeJson)
-					})
-					
-				})
-			})
-			//console.log(pokeMap)
-			//populatePokeCards(pokedata.results)
-		})
-	})
+async function getAPIData(url) {
+	try {
+		const response = await fetch(url)
+		const data = await response.json()
+		return data
+	}
+		catch (error) {
+			console.error(error)
+		}
 }
+
+//now, use the async getAPIData function
+getAPIData('https://pokeapi.co/api/v2/pokemon?offset=5&limit=50"').then((data) => {
+	for (const pokemon of data.results) {
+		getAPIData(pokemon.url).then((pokeData) => {
+			populatePokeCard(pokeData)
+		})
+	}
+})
 
 let pokemonGrid = document.querySelector('.pokemonGrid')
 
-getPokeData('https://pokeapi.co/api/v2/pokemon?offset=5&limit=50"')
+//getPokeData('https://pokeapi.co/api/v2/pokemon?offset=5&limit=50"')
 
-console.log(allPokemon)
 
-populatePokeCards(allPokemon)
 
-function populatePokeCards(pokeArray) {
-	pokeArray.forEach(pokeArray => {	
+function populatePokeCard(singlePokemon) {
 	let pokeScene = document.createElement('div')
 	pokeScene.className = 'scene'
 	let pokeCard = document.createElement('div')
@@ -36,7 +33,7 @@ function populatePokeCards(pokeArray) {
 	)
 	let pokeFront = document.createElement('div')
 	pokeFront.className = 'card__face card__face--front'
-	pokeFront.textContent = 'front'
+	pokeFront.textContent = singlePokemon.name
 	let pokeBack = document.createElement('div')
 	pokeBack.className = 'card__face card__face--back'
 	pokeBack.textContent = 'back'
@@ -45,14 +42,9 @@ function populatePokeCards(pokeArray) {
 	pokeCard.appendChild(pokeBack)
 	pokeScene.appendChild(pokeCard)
 	pokemonGrid.appendChild(pokeScene)
-})
 }
-/*<div class="scene">
-<div class="card">
-<div class="card__face card__face--front">front</div>
-<div class="card__face card__face--back">Back</div>
-</div>
-</div>*/
+
+
 
 
 
